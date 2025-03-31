ffcf1266100684fba52fcca6a22341a169282f95
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-03-30 22:59:42 -0400

    Create SectionLayoutRouter.ts

---
+++ server/SectionLayoutRouter.ts
@@ -0,0 +1,88 @@
+const express = require('express');  // CommonJS import style
+import DBConnector from "../db/dbConnector";
+import { Models } from "../db/initConnection";
+import { Request, Response } from 'express-serve-static-core'
+/**
+ * Defines the router which handles requests going to /api/SectionLayouts
+ * 
+ * Created By: Chris Morgan
+ */
+
+  const router = express.Router()
+
+
+  const SectionLayout = Models.SectionLayout
+
+  /**
+   * Get SectionLayouts index
+   */
+  router.get("/", async (req: Request, res: Response) => {
+    let records = await SectionLayout.findAll()
+
+    if (!records || records.length === 0) {
+      res.sendStatus(404);
+    }
+  
+    res.json(records.map(r => r.columns))
+    res.sendStatus(200)
+  })
+
+  /**
+   * Get SectionLayout details
+   */
+  router.get("/:id", async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id)
+    let record = await SectionLayout.find(id)
+  
+    if (!record) {
+      res.sendStatus(404)
+    }
+  
+    res.json(record.columns)
+    res.sendStatus(200)
+  })
+
+  /**
+   * Create new SectionLayout
+   */
+  router.put("/new", async (req: Request, res: Response) => {
+    let record = await SectionLayout.create(req.body)
+
+    record.save()
+  
+    res.json(record.columns)
+    res.sendStatus(200)
+  })
+
+
+  /**
+   * Update Page Layout with given ID
+   */
+  router.patch("/:id", async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id)
+    let record = SectionLayout.create(req.body)
+  
+    if (record.columns.id !== id) {
+      res.sendStatus(400)
+    }
+  
+    await record.save()
+  
+    res.sendStatus(200)
+  })
+
+  /**
+   * Delete SectionLayout with given ID
+   */
+  router.delete("/:id", async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id)
+    let record = await SectionLayout.find(id)
+    if (!record) {
+      res.sendStatus(404)
+    }
+  
+    record.delete()
+    res.sendStatus(200)
+  })
+
+export default router;
