bc0ac45cf6d152c3071e1996ca8d933ad4aa2bb2
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-03-30 23:12:06 -0400

    Create UserType.ts

---
+++ server/models/UserType.ts
@@ -0,0 +1,11 @@
+/**
+ * Represents a user type object from the database
+ * 
+ * Created By: Chris Morgan
+ */
+
+import { Model } from "./Model";
+
+export class UserType extends Model{
+  type_name?:string
+}
